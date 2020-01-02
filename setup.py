e1b2b45b5cd1c9c2deb6d9b2ffde82f692b32e9f
lihaitao <ahrli_c@163.com>
2020-01-02 15:48:32 +0800

    初始化项目1

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='fmz_tool',
-      version='1',
+      version='1.0',
       author="ahrli",
       author_email="ahrli_c@163.com",
-      url='',
+      url='https://github.com/Ahrli/fmz_tool',
       packages=["fmz_tool"],
        )
 
